c77abd9075ea68e562c28b2e21fb466d159f1318
Job Chumo <43841454+cjkipu@users.noreply.github.com>
2020-05-11 09:42:02 +0300

    0.1.2

--- app/src/main/java/com/jobchumo/memegram/Login.java
+++ app/src/main/java/com/jobchumo/memegram/Login.java
@@ -1,20 +1,38 @@
 package com.jobchumo.memegram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    protected EditText emaili, passwo;
+    protected ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emaili = (EditText) findViewById(R.id.emailaddress);
+        passwo = (EditText) findViewById(R.id.passw);
+        progressBar = new ProgressBar(this);
     }
 
     public void Log_in(View view) {
+        String email = emaili.getText().toString().trim();
+        String pas = passwo.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pas)) {
+            Toast.makeText(Login.this, "Empty fields!! \nPlease Enter all your details", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
     public void Sign_up(View view) {
--- app/src/main/java/com/jobchumo/memegram/SignUp.java
+++ app/src/main/java/com/jobchumo/memegram/SignUp.java
@@ -3,16 +3,42 @@ package com.jobchumo.memegram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+    protected EditText emailadd, pass, confpass;
+    protected ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        emailadd = (EditText) findViewById(R.id.email_address);
+        pass = (EditText) findViewById(R.id.password);
+        confpass = (EditText) findViewById(R.id.confirmpass);
+        progressBar = new ProgressBar(this);
     }
 
     public void signup(View view) {
+        String emaill = emailadd.getText().toString().trim();
+        String passwo = pass.getText().toString().trim();
+        String confirm = confpass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(emaill) || TextUtils.isEmpty(passwo) || TextUtils.isEmpty(confirm)){
+            Toast.makeText(SignUp.this, "Empty fields!! \nPlease Enter all your details", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (passwo.equals(confirm)){
+
+        }
+        else {
+            Toast.makeText(SignUp.this, "Passwords do not match!! \nPlease try again", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 }
