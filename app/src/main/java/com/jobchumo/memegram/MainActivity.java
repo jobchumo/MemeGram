b83876e3f4e9fb1cb9b9d2ac3d1b8ee32604aeaa
Job Chumo <43841454+cjkipu@users.noreply.github.com>
2020-05-12 19:23:03 +0300

    0.1.7

--- app/src/main/java/com/jobchumo/memegram/MainActivity.java
+++ app/src/main/java/com/jobchumo/memegram/MainActivity.java
@@ -1,8 +1,13 @@
 package com.jobchumo.memegram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFrag = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFrag = new HomeFragment();
+                            break;
+
+                        case R.id.nav_profile:
+                            selectedFrag = new ProfileFragment();
+                            break;
+
+                        case R.id.nav_search:
+                            selectedFrag = new SearchFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFrag).commit();
+
+                    return true;
+                }
+            };
 }
