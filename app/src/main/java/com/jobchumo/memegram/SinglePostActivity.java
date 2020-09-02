873d59c08b21a13b642c3c2b3e3211455f6a9893
Job Chumo <43841454+cjkipu@users.noreply.github.com>
2020-09-03 00:20:56 +0300

    0.3.0

---
+++ app/src/main/java/com/jobchumo/memegram/SinglePostActivity.java
@@ -0,0 +1,18 @@
+package com.jobchumo.memegram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SinglePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_post);
+    }
+
+    public void delete(View view) {
+    }
+}
